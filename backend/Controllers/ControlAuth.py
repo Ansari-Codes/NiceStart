e27cd405eb9bb29bac649e00f3f2c01fbd7be73a
Ansari-Codes <ansaricodes@gmail.com>
2025-10-24 16:54:22 +0500

    Controller started

--- backend/Controllers/ControlAuth.py
+++ backend/Controllers/ControlAuth.py
@@ -11,25 +11,42 @@ async def _validate(values: dict) -> dict:
     name = values.get("name")
     email = values.get("email")
     pswd = values.get("password")
+    conf = values.get("password")
     errors = {}
     if name:
         if not IsValidName(name):
-            errors['name'] = 'Name is invalid'
+            errors['name'] = 'Name is invalid.'
         elif not _is_unique(name, 'name'):
             errors['name'] = 'Name is already taken!'
     if email:
         if not IsValidEmail(email):
-            errors['email'] = 'Email is invalid'
+            errors['email'] = 'Email is invalid.'
         elif not _is_unique:
             errors['email'] = 'Email is already taken!'
     if not pswd:
-        errors['password'] = 'Password is required'
+        errors['password'] = 'Password is weak.'
     elif StrengthOfPswd(pswd or '') < 4:
-        errors['password'] = 'Password is weak!'
+        errors['password'] = 'Password is required!'
+    if not conf:
+        errors['confirm'] = 'Confirm password is required.'
+    elif not (pswd == conf):
+        errors['password'] = 'Passwords do not match!'
     return errors
 
-async def create(data: dict) -> tuple[bool, dict]:
+async def create(data: dict) -> dict:
     errors = _validate(data)
+    result = []
     if not errors:
-        result = await Auth().create(data)
-    return False, {}
+        try:
+            result = await Auth().create(data)
+            if not isinstance(result, (list, tuple)):
+                result = [result]
+        except Exception as e:
+            result = []
+            errors['Unknown'] = "Sorry, we cannot create your account!"
+    success = not errors
+    return {
+        "success": success,
+        "errors": errors,
+        "data": result
+    }
