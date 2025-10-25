d067194732300fc5eae2aae9ab2086effa741bb0
Ansari-Codes <ansaricodes@gmail.com>
2025-10-25 16:57:26 +0500

    Created Starter Kit

--- pages/PageDashboard.py
+++ pages/PageDashboard.py
@@ -1,9 +1,8 @@
-from UI import SoftBtn, Button, INIT_THEME
+from UI import SoftBtn, Label, Button, INIT_THEME
 from utils.Storage import clearUserStorage
 from utils import navigate
 
 async def create():
     INIT_THEME()
+    Label("TODO", clas="text-3xl underline text-blue-500")
     SoftBtn("LogOut", on_click=lambda: [clearUserStorage(), navigate('/')])
-    SoftBtn("Test", on_click=lambda: print("Testing"))
-    Button("Test")
